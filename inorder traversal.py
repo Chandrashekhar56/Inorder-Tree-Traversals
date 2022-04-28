ed70fc3aee3dc1b4beb0d2b29e4ae186bb2d1dab
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2022-04-28 11:59:20 +0530

    Add files via upload

---
+++ inorder traversal.py
@@ -0,0 +1,35 @@
+class Create_Node:
+    def __init__(self,value):
+        self.left_child = None
+        self.right_child = None
+        self.data_part = value   
+    def insert_value(self, value):
+        if self.data_part:            
+            if value < self.data_part:
+                if self.left_child is None:
+                    self.left_child = Create_Node(value)
+                else:
+                    self.left_child.insert_value(value)
+            elif value > self.data_part:
+                if self.right_child is None:
+                    self.right_child = Create_Node(value)
+                else:
+                    self.right_child.insert_value(value)
+        else:
+            self.data_part = value
+    def inorder(self, root_node):
+        a =[]
+        if root_node:
+            a = self.inorder(root_node.left_child)
+            a.append(root_node.data_part)
+            a = a + self.inorder(root_node.right_child)
+        return a
+
+root_node = Create_Node(10)
+root_node.insert_value(20)
+root_node.insert_value(5)
+root_node.insert_value(17)
+root_node.insert_value(2)
+root_node.insert_value(13)
+root_node.insert_value(7)
+print(root_node.inorder(root_node))
